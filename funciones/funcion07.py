da0a6714e748f1a99213254166507e81e8bdfedd
RAULRUIZ-cpu <raulinsecupower@gmail.com>
2026-09-21 21:28:33 -0700

    funcion07.py
    Implement function to check if age is adult.

---
+++ funciones/funcion07.py
@@ -0,0 +1,13 @@
+"""
+Escribe una función llamada es_mayor_de_edad(edad) que retorne "Mayor" si tiene 18 años o más, y "Menor" en caso contrario.
+
+def es_mayor_de_edad(edad):
+    if edad >= 18:
+        return "Mayor"
+    else:
+        return "Menor"
+
+print(es_mayor_de_edad(17))
+print(es_mayor_de_edad(18))
+print(es_mayor_de_edad(45))
+"""
