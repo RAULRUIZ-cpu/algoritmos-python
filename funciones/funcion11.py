7e80005092db8c0f0cef2a20062e6f8f5a083b76
RAULRUIZ-cpu <raulinsecupower@gmail.com>
2026-09-21 21:33:38 -0700

    funcion11.py
    Implement function to determine the largest of three numbers without using max().

---
+++ funciones/funcion11.py
@@ -0,0 +1,15 @@
+"""
+Diseña una función llamada mayor_de_tres(a, b, c) que determine cuál es el mayor utilizando operadores lógicos and y comparaciones >=, sin utilizar max().
+
+def mayor_de_tres(a, b, c):
+    if a >= b and a >= c:
+        return a
+    elif b >= a and b >= c:
+        return b
+    else:
+        return c
+
+print(mayor_de_tres(5, 12, 9))
+print(mayor_de_tres(20, 3, 1))
+print(mayor_de_tres(4, 4, 4))
+"""
