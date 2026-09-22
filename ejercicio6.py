94f4685a0b4036a94d2009e1c1be9e8a9d687b80
RAULRUIZ-cpu <raulinsecupower@gmail.com>
2026-09-21 20:54:21 -0700

    ejercicio6.py
    Implementa un script para calcular el costo de una llamada telefónica basado en su duración.

---
+++ ejercicio6.py
@@ -0,0 +1,11 @@
+"""
+Se requiere determinar el costo que tendrá realizar una llamada telefónica con base en el tiempo que dura la llamada y en el costo por minuto.
+
+Costo por minuto: $3.00 MXN
+
+minutos = float(input("Ingresa la duración de la llamada en minutos: "))
+
+costo = minutos * 3
+
+print("El costo de la llamada es: $", costo, "MXN")
+"""
