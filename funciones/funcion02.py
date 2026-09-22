cf812761dcbb86eeee04351e8d0ed0cd57b80e79
RAULRUIZ-cpu <raulinsecupower@gmail.com>
2026-09-21 21:21:53 -0700

    funcion02.py
    Implement function to sum two numbers and test cases.

---
+++ funciones/funcion02.py
@@ -0,0 +1,10 @@
+"""
+Escribe una función llamada sumar(a, b) que reciba dos números y devuelva su suma.
+
+def sumar(a, b):
+    return a + b
+
+print(sumar(8, 5))
+print(sumar(-3, 10))
+print(sumar(0, 0))
+"""
