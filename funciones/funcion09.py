1c93b0141c717256052b6308c89b930c2ac2a0de
RAULRUIZ-cpu <raulinsecupower@gmail.com>
2026-09-21 21:29:57 -0700

    funcion09.py

---
+++ funciones/funcion09.py
@@ -0,0 +1,15 @@
+"""
+Crea una función llamada obtener_signo(numero) que clasifique el número como "Positivo", "Negativo" o "Cero" utilizando if, elif y else.
+
+def obtener_signo(numero):
+    if numero > 0:
+        return "Positivo"
+    elif numero < 0:
+        return "Negativo"
+    else:
+        return "Cero"
+
+print(obtener_signo(12))
+print(obtener_signo(-8))
+print(obtener_signo(0))
+"""
