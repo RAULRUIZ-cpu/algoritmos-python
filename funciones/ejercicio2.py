541d003a0732c32890f7057ea6d3b1c961b96062
RAULRUIZ-cpu <raulinsecupower@gmail.com>
2026-09-21 19:30:33 -0700

    ejercicio2.py
    Added a script to convert Mexican pesos to dollars based on exchange rate.

--- funciones/ejercicio2.py
+++ funciones/ejercicio2.py
@@ -1,1 +1,10 @@
+"""
+Una empresa importadora desea determinar cuántos dólares puede adquirir con equis cantidad de dinero mexicano.
 
+pesos = float(input("Ingresa la cantidad de pesos mexicanos: "))
+tipo_cambio = float(input("Ingresa el tipo de cambio: "))
+
+dolares = pesos / tipo_cambio
+
+print("Puede adquirir", dolares, "dólares")
+"""
