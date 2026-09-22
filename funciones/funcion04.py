b793c8df5fb2619ba348519ad8af8ce52cc1d073
RAULRUIZ-cpu <raulinsecupower@gmail.com>
2026-09-21 21:25:27 -0700

    funcion04.py
    Adds a function to return double the input value.

---
+++ funciones/funcion04.py
@@ -0,0 +1,10 @@
+"""
+Crea una función llamada obtener_doble(n) que tome un número y devuelva su valor multiplicado por 2.
+
+def obtener_doble(n):
+    return n * 2
+
+print(obtener_doble(14))
+print(obtener_doble(0))
+print(obtener_doble(-5))
+"""
