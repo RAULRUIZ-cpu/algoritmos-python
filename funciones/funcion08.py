9a81161c6cb91c0ddd2666090359c9fc5ddcb945
RAULRUIZ-cpu <raulinsecupower@gmail.com>
2026-09-21 21:29:13 -0700

    funcion08.py
    Implement area_triangulo function to calculate area of triangle with validation.

---
+++ funciones/funcion08.py
@@ -0,0 +1,13 @@
+"""
+Diseña una función llamada area_triangulo(base, altura) que calcule el área mediante (base * altura) / 2 y valide que ambos valores sean mayores a cero.
+
+def area_triangulo(base, altura):
+    if base > 0 and altura > 0:
+        return (base * altura) / 2
+    else:
+        return 0
+
+print(area_triangulo(10, 5))
+print(area_triangulo(7, 4))
+print(area_triangulo(-2, 5))
+"""
