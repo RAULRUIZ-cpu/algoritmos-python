fe46dd62f2b286558c0cd2615d61a403e3a692d3
RAULRUIZ-cpu <raulinsecupower@gmail.com>
2026-09-21 21:31:06 -0700

    funcion10.py
    Adds a basic operation function that performs addition, subtraction, or multiplication based on the operation string provided.

---
+++ funciones/funcion10.py
@@ -0,0 +1,17 @@
+"""
+Implementa una función llamada operacion_basica(a, b, operacion) que realice suma, resta o multiplicación dependiendo del texto recibido.
+
+def operacion_basica(a, b, operacion):
+    if operacion == "suma":
+        return a + b
+    elif operacion == "resta":
+        return a - b
+    elif operacion == "multiplica":
+        return a * b
+    else:
+        return "Operación no válida"
+
+print(operacion_basica(6, 4, "resta"))
+print(operacion_basica(5, 3, "multiplica"))
+print(operacion_basica(10, 2, "raiz"))
+"""
