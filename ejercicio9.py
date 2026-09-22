1db2944cc2d47a2a3f1da21d99801414990d510b
RAULRUIZ-cpu <raulinsecupower@gmail.com>
2026-09-21 20:58:27 -0700

    ejercicio9.py

---
+++ ejercicio9.py
@@ -0,0 +1,11 @@
+"""
+Determina el promedio que obtendrá un alumno considerando que realiza tres exámenes, de los cuales el primero y el segundo tienen una ponderación de 25%, mientras que el tercero tiene una ponderación de 50%.
+
+examen1 = float(input("Ingresa la calificación del primer examen: "))
+examen2 = float(input("Ingresa la calificación del segundo examen: "))
+examen3 = float(input("Ingresa la calificación del tercer examen: "))
+
+promedio = (examen1 * 0.25) + (examen2 * 0.25) + (examen3 * 0.50)
+
+print("El promedio final es:", promedio)
+"""
