8353f1b4c033381ab876ae02d110c6bc902225be
RAULRUIZ-cpu <raulinsecupower@gmail.com>
2026-09-21 20:49:00 -0700

    Update ejercicio01.py

--- funciones/ejercicio01.py
+++ funciones/ejercicio01.py
@@ -1,15 +1,1 @@
-"""
-Repaso - problema 1
 
-Una empresa importadora desea determinar cuántos
-dólares puede adquirir con equis cantidad de dinero
-mexicano
-"""
-"""
-pesos = float(input("Ingresa la cantidad de pesos mexicanos: "))
-tipo_cambio = float(input("Ingresa el tipo de cambio (pesos por dólar): "))
-
-dolares = pesos / tipo_cambio
-
-print("Puedes adquirir:", dolares, "dólares")
-"""
