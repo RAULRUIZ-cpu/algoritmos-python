31dce06933d594851d76ac61956ab5c64f6d1205
RAULRUIZ-cpu <raulinsecupower@gmail.com>
2026-09-21 19:32:48 -0700

    ejercicio3.py
    Added a script to calculate parking charges based on hours used.

--- funciones/ejercicio3.py
+++ funciones/ejercicio3.py
@@ -1,1 +1,14 @@
+"""
+Un estacionamiento requiere determinar el cobro que debe aplicar a las personas que lo utilizan. Considere que el cobro es con base en las horas que lo disponen y que las fracciones de hora se toman como completas.
 
+import math
+
+horas = float(input("Ingresa las horas utilizadas: "))
+precio_hora = float(input("Ingresa el precio por hora: "))
+
+horas_cobradas = math.ceil(horas)
+total = horas_cobradas * precio_hora
+
+print("Horas cobradas:", horas_cobradas)
+print("Total a pagar: $", total)
+"""
