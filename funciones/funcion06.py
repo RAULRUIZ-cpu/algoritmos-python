0fa78927ab84eb0e35372e73f9dd274cf7e0b2c4
RAULRUIZ-cpu <raulinsecupower@gmail.com>
2026-09-21 21:27:42 -0700

    funcion06.py

---
+++ funciones/funcion06.py
@@ -0,0 +1,13 @@
+"""
+Crea una función llamada mayor_de_dos(a, b) que devuelva el número más grande sin usar max().
+
+def mayor_de_dos(a, b):
+    if a >= b:
+        return a
+    else:
+        return b
+
+print(mayor_de_dos(15, 27))
+print(mayor_de_dos(40, -10))
+print(mayor_de_dos(8, 8))
+"""
