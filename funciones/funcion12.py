4b8f319db6c12606162863e0668223744dc207cd
RAULRUIZ-cpu <raulinsecupower@gmail.com>
2026-09-21 21:34:36 -0700

    funcion12.py
    Adds a function to convert numerical grades to letter grades.

---
+++ funciones/funcion12.py
@@ -0,0 +1,18 @@
+"""
+Crea una función llamada calificar(nota) que reciba una calificación de 0 a 100 y retorne una letra según el rango.
+
+def calificar(nota):
+    if nota >= 90:
+        return "A"
+    elif nota >= 80:
+        return "B"
+    elif nota >= 70:
+        return "C"
+    else:
+        return "F"
+
+print(calificar(95))
+print(calificar(85))
+print(calificar(75))
+print(calificar(60))
+"""
