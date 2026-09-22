92cbde984dcd5f888ce8c4d845cb7c830ed9a26c
RAULRUIZ-cpu <raulinsecupower@gmail.com>
2026-09-21 20:53:37 -0700

    ejercicio4.py

---
+++ ejercicio4.py
@@ -0,0 +1,11 @@
+"""
+La compañía de autobuses “La Curva Loca” requiere determinar el costo que tendrá el boleto de un viaje sencillo, esto basado en los kilómetros por recorrer y en el costo por kilómetro.
+
+Costo por km: $80.00 MXN
+
+kilometros = float(input("Ingresa los kilómetros por recorrer: "))
+
+costo = kilometros * 80
+
+print("El costo del boleto es: $", costo, "MXN")
+"""
