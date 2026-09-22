bde3ac9b7184f1026dc1b8fa3330707a23ad3e83
RAULRUIZ-cpu <raulinsecupower@gmail.com>
2026-09-21 21:26:12 -0700

    funcion05.py

---
+++ funciones/funcion05.py
@@ -0,0 +1,10 @@
+"""
+Escribe una función llamada longitud_nombre(nombre) que reciba una cadena y retorne cuántas letras tiene usando len().
+
+def longitud_nombre(nombre):
+    return len(nombre)
+
+print(longitud_nombre("Lucía"))
+print(longitud_nombre("Sol"))
+print(longitud_nombre(""))
+"""
