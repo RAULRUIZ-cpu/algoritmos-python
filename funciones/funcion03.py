1153d1037ebf00fbe9fbe8c952933419b2969184
RAULRUIZ-cpu <raulinsecupower@gmail.com>
2026-09-21 21:23:19 -0700

    funcion03.py

---
+++ funciones/funcion03.py
@@ -0,0 +1,10 @@
+"""
+Diseña una función llamada repetir_texto(texto, veces) que devuelva la palabra repetida la cantidad de veces indicada.
+
+def repetir_texto(texto, veces):
+    return texto * veces
+
+print(repetir_texto("Eco", 3))
+print(repetir_texto("Hola", 1))
+print(repetir_texto("Ja", 4))
+"""
