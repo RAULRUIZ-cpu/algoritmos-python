48604855f112729fe609c6f78d9c7ee1b358d96d
RAULRUIZ-cpu <raulinsecupower@gmail.com>
2026-09-21 19:37:45 -0700

    ejercicio10.py
    Added a script to calculate the total cost of pencils based on quantity.

--- funciones/ejercicio10.py
+++ funciones/ejercicio10.py
@@ -1,1 +1,15 @@
+"""
+Determina cuánto se debe pagar por equis cantidad de lápices considerando que si son 1000 o más el costo es de $0.85; de lo contrario, el precio es de $0.90.
 
+cantidad = int(input("Ingresa la cantidad de lápices: "))
+
+if cantidad >= 1000:
+    precio = 0.85
+else:
+    precio = 0.90
+
+total = cantidad * precio
+
+print("Precio por lápiz: $", precio)
+print("Total a pagar: $", total)
+"""
