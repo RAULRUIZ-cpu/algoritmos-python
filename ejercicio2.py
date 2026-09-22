a55663db3a1169429e3d53b768d12d09d5183142
RAULRUIZ-cpu <raulinsecupower@gmail.com>
2026-09-21 20:50:54 -0700

    ejercicio2.py
    Add a program to calculate age based on birth year.

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,1 +1,11 @@
+"""
+Una empresa que contrata personal requiere determinar la edad de las personas que solicitan trabajo, pero cuando se les realiza la entrevista sólo se les pregunta el año en que nacieron.
+
+anio_nacimiento = int(input("Ingresa el año de nacimiento: "))
+anio_actual = int(input("Ingresa el año actual: "))
+
+edad = anio_actual - anio_nacimiento
+
+print("La edad de la persona es:", edad, "años")
+"""
 
