bcfa149d706009271b293bf7bf072cc4302d812d
RAULRUIZ-cpu <raulinsecupower@gmail.com>
2026-09-21 21:20:33 -0700

    funcion1.py

---
+++ funciones/funcion1.py
@@ -0,0 +1,10 @@
+"""
+Crea una función llamada saludar(nombre) que reciba un nombre como cadena de texto y retorne "Hola, <nombre>!".
+
+def saludar(nombre):
+    return "Hola, " + nombre + "!"
+
+print(saludar("Carlos"))
+print(saludar("Ana"))
+print(saludar("Mundo"))
+"""
